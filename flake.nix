b572131027b9dab93cb63a089f28b93a43fc5c88
hugosenari <hugosenari@gmail.com>
2022-03-12 03:17:51 -0300

    chore: re index grp

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim port of Python's grp module for working with the UNIX group database file'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."grp-master".url = "path:./master";
+  inputs."grp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."grp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
